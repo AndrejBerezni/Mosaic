773a5785ea2c5216254975ab46d5b1711177c37d
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2023-09-14 11:44:20 +0100

    create first version of Navbar
    add additional styles to first version of navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,51 @@
+import "./Navbar.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Image from "react-bootstrap/Image";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import logo from "../../assets/mosaic.png";
+import { MoonStars } from "react-bootstrap-icons";
+import { CurrencyEuro } from "react-bootstrap-icons";
+import { CurrencyDollar } from "react-bootstrap-icons";
+import { CurrencyPound } from "react-bootstrap-icons";
+
 function Nabvar() {
   return (
-    <>
-      <h1>Navbar</h1>
-    </>
+    <Navbar expand="md" sticky="top" id="navbar">
+      <Container>
+        <Navbar.Brand href="#home" id="title">
+          <Image src={logo} id="navbar-logo" />
+          Mosaic
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#link" className="navbar-item">
+              <MoonStars />
+            </Nav.Link>
+            <NavDropdown
+              title="Currency"
+              id="responsive-nav-dropdown"
+              className="navbar-item"
+            >
+              <NavDropdown.Item href="" className="currency-selector">
+                EUR <CurrencyEuro />
+              </NavDropdown.Item>
+              <NavDropdown.Item href="" className="currency-selector">
+                USD <CurrencyDollar />
+              </NavDropdown.Item>
+              <NavDropdown.Item href="" className="currency-selector">
+                GBP <CurrencyPound />
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#home" className="navbar-item">
+              Sign Out
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
