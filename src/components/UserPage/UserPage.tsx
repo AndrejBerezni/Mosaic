aab936d7f5029c24e86325bc16ebe7becbfb83ac
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2023-09-15 12:38:09 +0100

    create Total component

--- src/components/UserPage/UserPage.tsx
+++ src/components/UserPage/UserPage.tsx
@@ -3,6 +3,7 @@ import AssetBar from "./AssetBar/AssetBar";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import AddAsset from "./AddAsset/AddAsset";
+import Total from "./Total/Total";
 
 function UserPage() {
   return (
@@ -14,8 +15,13 @@ function UserPage() {
           <AssetBar assetName={"ethereum"} units={66} totalValue={76400} />
           <AssetBar assetName={"xrp"} units={3} totalValue={3.24} />
           <AssetBar assetName={"cardano"} units={408} totalValue={230} />
+          <AssetBar assetName={"bitcoin"} units={2} totalValue={42600} />
+          <AssetBar assetName={"ethereum"} units={66} totalValue={76400} />
+          <AssetBar assetName={"xrp"} units={3} totalValue={3.24} />
+          <AssetBar assetName={"cardano"} units={408} totalValue={230} />
           <AddAsset />
         </Col>
+        <Total />
       </Container>
     </>
   );
