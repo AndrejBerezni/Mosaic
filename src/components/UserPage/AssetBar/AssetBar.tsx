b1fea7d2019deb31bb19a8b7f5233c2418b12f39
Francisco Carvalhal <f.sgcarvalhal@gmail.com>
2023-09-15 14:15:18 +0100

    Options Btn display

--- src/components/UserPage/AssetBar/AssetBar.tsx
+++ src/components/UserPage/AssetBar/AssetBar.tsx
@@ -4,6 +4,8 @@ import Button from "react-bootstrap/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 import CardText from "react-bootstrap/CardText";
 import Table from "react-bootstrap/Table";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
 
 function AssetBar({ assetName, units, totalValue }) {
   return (
@@ -17,14 +19,24 @@ function AssetBar({ assetName, units, totalValue }) {
         </tr>
       </Table>
       <ButtonGroup aria-label="Basic example">
-        <Button className="asset-bar-btn" variant="warning">
-          Edit
+        <Button className="add-units-button asset-bar-btn">Add Units</Button>
+        <Button className="remove-units-button asset-bar-btn">
+          Remove Units
         </Button>
-        <Button className="asset-bar-btn" variant="danger">
-          Remove
-        </Button>
-        <Button className="add-units-button asset-bar-btn">+</Button>
-        <Button className="remove-units-button asset-bar-btn">-</Button>
+
+        <DropdownButton
+          className="asset-bar-btn"
+          as={ButtonGroup}
+          title="Options"
+          id="bg-nested-dropdown"
+        >
+          <Dropdown.Item eventKey="1" className="asset-bar-btn">
+            <Button variant="outline-warning">Edit</Button>
+          </Dropdown.Item>
+          <Dropdown.Item eventKey="2" className="asset-bar-btn">
+            <Button variant="outline-danger ">Delete</Button>
+          </Dropdown.Item>
+        </DropdownButton>
       </ButtonGroup>
     </Card>
   );
