6f7f46a510bbce9764cc613ac8370232c8602cbe
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2023-09-16 18:07:43 +0100

    add initial form elements to NewAssetForm

--- src/components/Forms/NewAssetForm/NewAssetForm.tsx
+++ src/components/Forms/NewAssetForm/NewAssetForm.tsx
@@ -1,5 +1,8 @@
+import "./NewAssetForm.css";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
 import { hideForm } from "../../../actions/showFormActions";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../../reducers/combineReducers";
@@ -22,13 +25,36 @@ function NewAssetForm() {
       <Modal.Header closeButton>
         <Modal.Title>Add New Asset to Portfolio</Modal.Title>
       </Modal.Header>
-      <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+      <Form className="p-3">
+        <FloatingLabel label="Asset Type" className="my-3">
+          <Form.Select aria-label="asset type">
+            <option value="stocks">Stocks</option>
+            <option value="metal">Noble Metal</option>
+            <option value="cash">Cash</option>
+            <option value="crypto">Crypto</option>
+          </Form.Select>
+        </FloatingLabel>
+        <FloatingLabel label="Asset Name" className="my-3">
+          <Form.Control type="text" as="input" required />
+        </FloatingLabel>
+        <FloatingLabel label="Number of Units" className="my-3">
+          <Form.Control
+            type="number"
+            as="input"
+            required
+            defaultValue={1}
+            min={0.00001}
+            step={"any"}
+          />
+        </FloatingLabel>
+      </Form>
       <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose}>
-          Close
-        </Button>
-        <Button variant="primary" onClick={handleClose}>
-          Save Changes
+        <Button
+          variant="primary"
+          onClick={handleClose}
+          className="submit-form-btn"
+        >
+          Save
         </Button>
       </Modal.Footer>
     </Modal>
